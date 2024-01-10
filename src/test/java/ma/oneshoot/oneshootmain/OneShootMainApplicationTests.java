e873f90e8bce4f8bb87433821c958ee8df5679f6
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-10 01:13:34 +0100

    disable tests for now

--- src/test/java/ma/oneshoot/oneshootmain/OneShootMainApplicationTests.java
+++ src/test/java/ma/oneshoot/oneshootmain/OneShootMainApplicationTests.java
@@ -1,18 +1,9 @@
 package ma.oneshoot.oneshootmain;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 class OneShootMainApplicationTests {
 
-	@Test
-	@Disabled
-	void contextLoads() {
-	}
 
 }

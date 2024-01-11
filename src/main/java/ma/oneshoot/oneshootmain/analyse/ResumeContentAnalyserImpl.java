7e32d7f5e6420f107a974cb2600af83243123e87
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-11 01:26:27 +0100

    new: simple extracted text analized function

--- src/main/java/ma/oneshoot/oneshootmain/analyse/ResumeContentAnalyserImpl.java
+++ src/main/java/ma/oneshoot/oneshootmain/analyse/ResumeContentAnalyserImpl.java
@@ -3,6 +3,8 @@ package ma.oneshoot.oneshootmain.analyse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -10,7 +12,25 @@ import java.util.Map;
 public class ResumeContentAnalyserImpl {
 
 
-    public void analyse(Map<String, String> extractedObjectFromPdf) {
+    public List<AnalysisReport> analyse(Map<String, String> extractedObjectFromPdf) {
+        List<AnalysisReport> analysisReports = new ArrayList<>();
         log.info("Analyse extracted text");
+        for (Map.Entry<String, String> entry : extractedObjectFromPdf.entrySet()) {
+            AnalysisReport analysisReport = new AnalysisReport();
+            analysisReport.setHeader(entry.getValue());
+            analysisReport.setBody(entry.getValue());
+            if(entry.getKey().isEmpty()){
+                analysisReport.addRemark("Empty body");
+            }
+            if(entry.getKey().length() < 50){
+                            analysisReport.addRemark("Body is too short");
+            }
+            if(entry.getKey().length() > 1000){
+                            analysisReport.addRemark("Body is too long");
+            }
+            analysisReports.add(analysisReport);
+        }
+        log.info("Text analysed successfully");
+        return analysisReports;
     }
 }

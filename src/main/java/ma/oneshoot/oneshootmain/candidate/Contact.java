7d646cbdf96b090eae35bfc56a593fd650adba13
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-01-17 01:22:40 +0100

    base entities for candidate processing init

--- src/main/java/ma/oneshoot/oneshootmain/candidate/Contact.java
+++ src/main/java/ma/oneshoot/oneshootmain/candidate/Contact.java
@@ -1,2 +1,17 @@
-package ma.oneshoot.oneshootmain.candidate;public class Contact {
+package ma.oneshoot.oneshootmain.candidate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+    String city  ;
+    String email;
+    String website ;
+
 }
--- src/main/java/ma/oneshoot/oneshootmain/candidate/Experience.java
+++ src/main/java/ma/oneshoot/oneshootmain/candidate/Experience.java
@@ -1,7 +1,15 @@
 package ma.oneshoot.oneshootmain.candidate;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@Builder
+@AllArgsConstructor @NoArgsConstructor
 public class Experience {
     String entreprise ;
     String title ;

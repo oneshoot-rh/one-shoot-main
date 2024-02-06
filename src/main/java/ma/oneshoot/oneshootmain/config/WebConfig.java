af7ecff8ac8af4aaec4ca0dffcfbbfa9501d74ac
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-02-06 21:38:13 +0100

    new: multi-tenancy config

--- src/main/java/ma/oneshoot/oneshootmain/config/WebConfig.java
+++ src/main/java/ma/oneshoot/oneshootmain/config/WebConfig.java
@@ -1,7 +1,8 @@
 package ma.oneshoot.oneshootmain.config;
 
 import lombok.RequiredArgsConstructor;
-import ma.oneshoot.oneshootmain.tenant.db.TenantInterceptor;
+import ma.oneshoot.oneshootmain.utils.interceptors.ClientSpecificRequestInterceptor;
+import ma.oneshoot.oneshootmain.utils.interceptors.TenantInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final TenantInterceptor tenantInterceptor;
+    private final ClientSpecificRequestInterceptor clientSpecificRequestInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //registry.addInterceptor(clientSpecificRequestInterceptor).order(1);
         registry.addInterceptor(tenantInterceptor);
     }
 }

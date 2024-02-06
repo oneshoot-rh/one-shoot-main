72e8cc49556c0d8b6a1e881a269b030499f3ebd0
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-02-06 21:37:47 +0100

    new: multi-tenancy config

--- src/main/java/ma/oneshoot/oneshootmain/config/MultitenancyConfig.java
+++ src/main/java/ma/oneshoot/oneshootmain/config/MultitenancyConfig.java
@@ -1,52 +1,48 @@
 package ma.oneshoot.oneshootmain.config;
 
 
+import ma.oneshoot.oneshootmain.OneShootMainApplication;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Environment;
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-//@Configuration
+@Configuration
 public class MultitenancyConfig {
 
-    // Multi-Tenant LocalSessionFactoryBean that is used for multi-tenant sessions
     @Bean
-    LocalSessionFactoryBean sessionFactory(DataSource dataSource){
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setDataSource(dataSource);
-        localSessionFactoryBean.setPackagesToScan("ma.oneshoot.oneshootmain.tenant.db");
-        localSessionFactoryBean.setHibernateProperties(hibernateProperties());
-        return localSessionFactoryBean;
-
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
     }
 
-    private Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true");
-        hibernateProperties.setProperty("hibernate.multiTenancy", "SCHEMA");
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "NONE");
-        return hibernateProperties;
-    }
-    // Non Multi-Tenant LocalSessionFactoryBean that is used to create schemas for tenants
     @Bean
-    LocalSessionFactoryBean sessionFactorySchemaManager(DataSource dataSource){
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setDataSource(dataSource);
-        localSessionFactoryBean.setPackagesToScan("ma.oneshoot.oneshootmain.tenant.db");
-        localSessionFactoryBean.setHibernateProperties(hibernatePropertiesSchemaManager());
-        return localSessionFactoryBean;
-    }
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaProperties jpaProperties,
+                                                                       MultiTenantConnectionProvider multiTenantConnectionProvider, CurrentTenantIdentifierResolver tenantIdentifierResolver) {
 
-    private Properties hibernatePropertiesSchemaManager() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true");
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create");
-        hibernateProperties.setProperty("hibernate.multiTenancy", "NONE");
-        return hibernateProperties;
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan(OneShootMainApplication.class.getPackage().getName());
+        em.setJpaVendorAdapter(jpaVendorAdapter());
+
+        Map<String, Object> jpaPropertiesMap = new HashMap<>(jpaProperties.getProperties());
+/*
+        jpaPropertiesMap.put(Environment.MULTI_TENANT, MultiTenancyStrategy.SCHEMA); // dipreciated
+*/
+        jpaPropertiesMap.put(Environment.MULTI_TENANT_CONNECTION_PROVIDER, multiTenantConnectionProvider);
+        jpaPropertiesMap.put(Environment.MULTI_TENANT_IDENTIFIER_RESOLVER, tenantIdentifierResolver);
+        em.setJpaPropertyMap(jpaPropertiesMap);
+
+
+        return em;
     }
 }

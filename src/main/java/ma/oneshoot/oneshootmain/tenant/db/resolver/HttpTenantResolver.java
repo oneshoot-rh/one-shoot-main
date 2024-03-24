ac53b0712c25b98e48215948081fdb39f734b636
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-24 17:26:22 +0000

    using default http header to extract tenant id

--- src/main/java/ma/oneshoot/oneshootmain/tenant/db/resolver/HttpTenantResolver.java
+++ src/main/java/ma/oneshoot/oneshootmain/tenant/db/resolver/HttpTenantResolver.java
@@ -13,6 +13,8 @@ public class HttpTenantResolver implements TenantResolver<HttpServletRequest> {
     public String resolveTenantId(HttpServletRequest request) {
         String forwardedHeader = request.getHeader("forwarded");
         String quoted = "host=\"";
-        return (((forwardedHeader.split(forwardedHeader.contains(quoted) ? quoted : "host=")[1]).split(";"))[0]).split("\\.")[0];
+        request.getHeaderNames().asIterator().forEachRemaining(System.out::println);
+       // return (((forwardedHeader.split(forwardedHeader.contains(quoted) ? quoted : "host=")[1]).split(";"))[0]).split("\\.")[0];
+       return request.getHeader("x-tenant-id");
     }
 }

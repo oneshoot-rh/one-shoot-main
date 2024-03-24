2980425700dade14c85f20a8071086fd58335552
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-24 17:23:41 +0000

    updated: default schema name

--- src/main/java/ma/oneshoot/oneshootmain/tenant/db/TenantContext.java
+++ src/main/java/ma/oneshoot/oneshootmain/tenant/db/TenantContext.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public final class TenantContext {
 
     private static final ThreadLocal<String> tenantId = new InheritableThreadLocal<>();
-    public static final String DEFAULT_TENANT_ID = "one_shoot_main";
+    public static final String DEFAULT_TENANT_ID = "oneshootmain";
 
 
     public static void setTenantId(String tenant) {

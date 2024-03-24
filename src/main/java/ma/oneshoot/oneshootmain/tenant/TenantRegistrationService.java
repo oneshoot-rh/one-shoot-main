008d4e0cbb5f0915243d3587adda1a3bdf0540c4
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-24 17:27:27 +0000

    update login of tenant registration

--- src/main/java/ma/oneshoot/oneshootmain/tenant/TenantRegistrationService.java
+++ src/main/java/ma/oneshoot/oneshootmain/tenant/TenantRegistrationService.java
@@ -1,8 +1,14 @@
 package ma.oneshoot.oneshootmain.tenant;
 
 import lombok.RequiredArgsConstructor;
+import ma.oneshoot.oneshootmain.subscription.FreeTrial;
+import ma.oneshoot.oneshootmain.subscription.FreeTrialRepository;
+import ma.oneshoot.oneshootmain.subscription.NewSubscriptionEvent;
+import ma.oneshoot.oneshootmain.subscription.SubscriptionPayment;
+import ma.oneshoot.oneshootmain.subscription.SubscriptionPaymentRepository;
 import ma.oneshoot.oneshootmain.utils.SubscriptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -21,19 +29,40 @@ public class TenantRegistrationService {
     private final TenantService tenantService;
     private final SubscriptionRepository subscriptionRepository;
     private final PasswordEncoder passwordEncoder;
+    private final FreeTrialRepository freeTrialRepository;
+    private final SubscriptionPaymentRepository subscriptionPaymentRepository;
+    @Autowired
+    private final ApplicationEventPublisher applicationEventPublisher;
 
 
 
 
 
     @Transactional(rollbackFor = Exception.class)
-    public Tenant registerTenant(Tenant tenant,SubscriptionType subscriptionType) {
+    public Tenant registerTenant(Tenant tenant,SubscriptionType subscriptionType, boolean isDemo) {
         Objects.requireNonNull(tenant, "Tenant object cannot be null");
         Objects.requireNonNull(subscriptionType, "Subscription type cannot be null");
         Objects.requireNonNull(tenant.getUsers(), "Tenant should have at least one user");
         tenant.getUsers().forEach(user -> user.setPassword(passwordEncoder.encode(user.getPassword())));
         userRepository.saveAll(tenant.getUsers());
-        tenantService.saveTenant(tenant);
+        var savedTenant = tenantService.saveTenant(tenant);
+        if (isDemo) {
+            var freetrial = FreeTrial.builder()
+            .endDate(LocalDateTime.now().plus(10, ChronoUnit.DAYS))
+            .tenant(savedTenant)
+            .organizationName(tenant.getOrganizationName())
+            .build();
+            freeTrialRepository.save(freetrial);
+        }
+        // for demo payment delayed to 10 days
+        var subscriptionPayment = SubscriptionPayment.builder()
+                .tenant(savedTenant)
+                .organizationName(tenant.getOrganizationName())
+                .paymentDelay(LocalDateTime.now().plusDays(SubscriptionUtil.getPaymentDelayInDays(subscriptionType)))
+                .amount(SubscriptionUtil.getSubscriptionPrice(subscriptionType))
+                .reference(UUID.randomUUID())
+                .build();
+        subscriptionPaymentRepository.save(subscriptionPayment); 
         Subscription subscription = Subscription.builder()
                 .tenant(tenant)
                 .startDate(LocalDateTime.now())
@@ -43,6 +72,13 @@ public class TenantRegistrationService {
                 .build();
         subscriptionRepository.save(subscription);
         tenantService.initDataBase(tenant.getOrganizationName());
+        NewSubscriptionEvent newSubscriptionEvent = new NewSubscriptionEvent(this, tenant.domainName);
+        applicationEventPublisher.publishEvent(newSubscriptionEvent);
         return tenant;
+    }
+
+
+    public List<Subscription> getSubscriptions(){
+        return subscriptionRepository.findAll();
     }
 }

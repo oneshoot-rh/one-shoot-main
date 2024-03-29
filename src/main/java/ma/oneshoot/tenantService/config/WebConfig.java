40cb887bb4a4c6c0423735679b8ccc87cdb3760d
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-29 05:13:07 +0000

    config rest template

--- src/main/java/ma/oneshoot/tenantService/config/WebConfig.java
+++ src/main/java/ma/oneshoot/tenantService/config/WebConfig.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import ma.oneshoot.tenantService.utils.interceptors.ClientSpecificRequestInterceptor;
 import ma.oneshoot.tenantService.utils.interceptors.TenantInterceptor;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //registry.addInterceptor(clientSpecificRequestInterceptor).order(1);
         registry.addInterceptor(tenantInterceptor);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }

4b643dc00691250e92ef9b79dee3bb8ff3c27fdf
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-29 05:11:38 +0000

    keycloak admin rest client configuratio

--- src/main/java/ma/oneshoot/tenantService/config/GlobalConfig.java
+++ src/main/java/ma/oneshoot/tenantService/config/GlobalConfig.java
@@ -1,15 +1,53 @@
 package ma.oneshoot.tenantService.config;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @Configuration
 public class GlobalConfig {
+
+    @Value("${keycloak.adminClientId}")
+    private String adminClientId;
+
+    @Value("${keycloak.adminClientSecret}")
+    private String adminClientSecret;
+
+    @Value("${keycloak.serverUrl}")
+    private String serverUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Bean 
+    Keycloak keycloak() {
+        System.out.println("serverUrl: " + serverUrl);
+        System.out.println("realm: " + realm);
+        System.out.println("adminClientId: " + adminClientId);
+        System.out.println("adminClientSecret: " + adminClientSecret);
+        return KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .clientId(adminClientId)
+                .clientSecret(adminClientSecret)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                // .resteasyClient(new ResteasyClientBuilder().build())
+                .build();   
+    }
+
+    
 }

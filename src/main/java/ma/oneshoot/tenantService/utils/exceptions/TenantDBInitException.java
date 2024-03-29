9b7cd46b85ee7a9a29f4c079cda9d86234e2a50b
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-29 05:16:16 +0000

    added custom exception

---
+++ src/main/java/ma/oneshoot/tenantService/utils/exceptions/TenantDBInitException.java
@@ -0,0 +1,7 @@
+package ma.oneshoot.tenantService.utils.exceptions;
+
+public class TenantDBInitException extends Exception{
+    public TenantDBInitException(String message){
+        super(message);
+    }
+}
